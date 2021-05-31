a055d96ab57acf6d0683b971a9a5ec50e9da70ea
Sumit Jamnani <39671178+SumitJamnani@users.noreply.github.com>
2021-06-01 03:57:12 +0530

    commited on 1-6-2021 by Sumit

--- app/src/main/java/com/example/sumit/srms/StudentUpdateProfile.java
+++ app/src/main/java/com/example/sumit/srms/StudentUpdateProfile.java
@@ -1,13 +1,147 @@
 package com.example.sumit.srms;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
 
 public class StudentUpdateProfile extends AppCompatActivity {
+
+    private static final int PERMISSION_REQUEST_CODE = 1;
+    private static final int REQUEST_GALLERY = 200;
+    String file_path = null;
+
+    TextView file_name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_update_profile);
+        Button upload_file = findViewById(R.id.btn_img_file);
+        upload_file.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                if(Build.VERSION.SDK_INT>=23)
+                {
+                    if(checkPermission())
+                    {
+                        filepicker();
+                    }
+                    else
+                    {
+                        requestPermission();
+                    }
+                }
+
+                else
+                {
+                    filepicker();
+                }
+            }
+        }
+        );
+
+        file_name = findViewById(R.id.filename);
+    }
+
+    private void filepicker()
+    {
+        Toast.makeText(StudentUpdateProfile.this, "File Picker Called!!", Toast.LENGTH_SHORT).show();
+
+        // Pick File
+        Intent opengallery = new Intent(Intent.ACTION_PICK);
+        opengallery.setType("image/*");
+        startActivityForResult(opengallery, REQUEST_GALLERY);
+
+
+    }
+
+    private void requestPermission()
+    {
+        if(ActivityCompat.shouldShowRequestPermissionRationale(StudentUpdateProfile.this, Manifest.permission.READ_EXTERNAL_STORAGE))
+        {
+            Toast.makeText(StudentUpdateProfile.this, "Please Give Permission To Upload File", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            ActivityCompat.requestPermissions(StudentUpdateProfile.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    private boolean checkPermission()
+    {
+        int result = ContextCompat.checkSelfPermission(StudentUpdateProfile.this, Manifest.permission.READ_EXTERNAL_STORAGE);
+        if(result == PackageManager.PERMISSION_GRANTED)
+        {
+            return  true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode)
+        {
+            case PERMISSION_REQUEST_CODE:
+                if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                {
+                    Toast.makeText(StudentUpdateProfile.this, "Permission Successful", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Toast.makeText(StudentUpdateProfile.this, "Permission Failed", Toast.LENGTH_SHORT).show();
+                }
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQUEST_GALLERY && resultCode == Activity.RESULT_OK)
+        {
+            String filePath  = getRealPathFromUrl(data.getData(), StudentUpdateProfile.this);
+            Toast.makeText(StudentUpdateProfile.this, "Path : " + filePath, Toast.LENGTH_SHORT).show();
+
+            this.file_path = filePath;
+            File file = new File(filePath);
+            file_name.setText(file.getName());
+        }
+    }
+
+    public String getRealPathFromUrl(Uri url, Activity activity)
+    {
+        Cursor cursor = activity.getContentResolver().query(url, null,null,null,null);
+        if(cursor==null)
+        {
+            return url.getPath();
+        }
+        else
+        {
+            cursor.moveToFirst();
+            int id = cursor.getColumnIndex(MediaStore.Images.ImageColumns._ID);
+            return  cursor.getString(id);
+        }
     }
 }
