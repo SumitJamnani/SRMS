0b5dd58d5e366b8624605e3f62fe4f8cd69a09e4
Sumit Jamnani <39671178+SumitJamnani@users.noreply.github.com>
2021-06-23 16:20:56 +0530

    Commited on 23-6-2021 by Sumit

---
+++ app/src/main/java/com/example/sumit/srms/Home.java
@@ -0,0 +1,39 @@
+package com.example.sumit.srms;
+
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class Home extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        //Side Drawer Code
+        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.admin_drawer);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //End Side Drawer Code
+    }
+
+    //Side drawer open close related method
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(actionBarDrawerToggle.onOptionsItemSelected(item))
+        {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    //End Side drawer open close related method
+}
--- app/src/main/java/com/example/sumit/srms/SplashScreen.java
+++ app/src/main/java/com/example/sumit/srms/SplashScreen.java
@@ -38,7 +38,7 @@ public class SplashScreen extends AppCompatActivity {
             @Override
             public void run() {
 
-                Intent intent = new Intent(SplashScreen.this, StudentsResults.class);
+                Intent intent = new Intent(SplashScreen.this, LoginActivity.class);
                 startActivity(intent);
                 finish();
             }
